441cfe1059253f191639b42f075e0cd1643124d6
Yunus Emre Şencan <yunusemresencan0@gmail.com>
2022-09-18 12:34:40 +0300

    Change title

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
   return (
     <div>
       <Header>
-        <TitleLink to="/">nba-stats</TitleLink>{" "}
+        <TitleLink to="/">NBA-Stats</TitleLink>{" "}
       </Header>
       <StyledApp>
         <Outlet />
